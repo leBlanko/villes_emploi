0a2a9e480195cbbd94f72234d57abe3e85dc9274
malfoy <tito.tati@me.com>
2016-04-14 10:24:04 +0200

    Reglages rendus graphique sur smartphone, creation début page profil

--- app/build/generated/source/r/debug/com/facebook/R.java
+++ app/build/generated/source/r/debug/com/facebook/R.java
@@ -100,7 +100,7 @@ public final class R {
 		public static final int inline = 0x7f0c0040;
 		public static final int large = 0x7f0c0041;
 		public static final int left = 0x7f0c0024;
-		public static final int messenger_send_button = 0x7f0c0090;
+		public static final int messenger_send_button = 0x7f0c0092;
 		public static final int normal = 0x7f0c000b;
 		public static final int open_graph = 0x7f0c003a;
 		public static final int page = 0x7f0c003b;
--- app/src/main/java/com/malfoy/leblanko/villes_emploi/Fragment/Profil.java
+++ app/src/main/java/com/malfoy/leblanko/villes_emploi/Fragment/Profil.java
@@ -1,30 +1,48 @@
 package com.malfoy.leblanko.villes_emploi.Fragment;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 import com.malfoy.leblanko.villes_emploi.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class Profil extends Fragment {
 
 
-    public Profil() {
-        // Required empty public constructor
-    }
+    private WebView descView;
 
+    public Profil() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profil, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_profil, container, false);
+
+        descView = (WebView) view.findViewById(R.id.descProfil);
+        descView.setBackgroundColor(getResources().getColor(R.color.focusButton));
+        justifyWebView(descView, R.string.descProfil);
+
+
+        return view;
+    }
+
+    //Methode qui va permettre de justifier le texte dans la vue
+    public void justifyWebView(WebView webView, int id)
+    {
+        String text;
+        String getDesc = getResources().getString(id);
+        text = "<html><body><p align=\"justify\" style=\"color:white;\" >";
+        text+= getDesc;
+        text+= "</p></body></html>";
+        webView.loadData(text, "text/html; charset=UTF-8",null);
     }
 
 }
