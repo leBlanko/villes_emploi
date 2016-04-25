ba632f1ab22910717152dfe4a826f40c9c67766e
malfoy <tito.tati@me.com>
2016-04-25 12:31:31 +0200

    -- Retrait temporaire de la page <Services> du menu; ++ Ajout de la page <Coach> au menu, creation de celle-ci, presentation du coach avec redirection vers le site web pour s abonner

--- app/src/main/java/com/malfoy/leblanko/villes_emploi/Fragment/Tribu.java
+++ app/src/main/java/com/malfoy/leblanko/villes_emploi/Fragment/Tribu.java
@@ -1,11 +1,14 @@
 package com.malfoy.leblanko.villes_emploi.Fragment;
 
 
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.malfoy.leblanko.villes_emploi.R;
 
@@ -24,6 +27,7 @@ public class Tribu extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_tribu, container, false);
     }
 
